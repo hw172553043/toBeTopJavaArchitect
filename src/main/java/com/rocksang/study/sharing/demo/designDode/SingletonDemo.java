e550a33b7f8f5389c43f761e93ba3eee37299576
sangbowei <rock.sang@oyohotels.cn>
2019-11-28 16:54:17 +0800

    优化

--- src/main/java/com/rocksang/study/sharing/demo/designDode/SingletonDemo.java
+++ src/main/java/com/rocksang/study/sharing/demo/designDode/SingletonDemo.java
@@ -5,13 +5,13 @@ package com.rocksang.study.sharing.demo.designDode;
  *
  * @blame Android Team
  */
-public class Singleton {
+public class SingletonDemo {
 
     // 饿汉式
-    private static Singleton hangerSingleton = new Singleton();
-    private Singleton() {
+    private static SingletonDemo hangerSingleton = new SingletonDemo();
+    private SingletonDemo() {
     }
-    protected static Singleton getHungerInstance() {
+    protected static SingletonDemo getHungerInstance() {
         return hangerSingleton;
     }
 
@@ -19,12 +19,12 @@ public class Singleton {
     // 懒汉式
     // 优点：延迟加载（需要的时候才去加载）,适合单线程操作
     // 缺点： 线程不安全，在多线程中很容易出现不同步的情况，如在数据库对象进行的频繁读写操作时。
-    private static Singleton lazySingleton = null;
+    private static SingletonDemo lazySingleton = null;
     //    private Singleton() {
     //    }
-    public static Singleton getLazyInstance() {
+    public static SingletonDemo getLazyInstance() {
         if (null == lazySingleton) {
-            lazySingleton = new Singleton();
+            lazySingleton = new SingletonDemo();
         }
         return lazySingleton;
     }
@@ -32,14 +32,14 @@ public class Singleton {
     //  双重线程检查模式
     //  优点：线程安全，支持延时加载，调用效率高
     //  缺点： 写法复杂，不简洁
-    private static Singleton doubleCheckSingleton = null;
+    private static SingletonDemo doubleCheckSingleton = null;
     //    private Singleton() {
     //    }
-    public static Singleton getDoubleCheckInstance() {
+    public static SingletonDemo getDoubleCheckInstance() {
         if (null == doubleCheckSingleton) {
-            synchronized (Singleton.class) {
+            synchronized (SingletonDemo.class) {
                 if (null == doubleCheckSingleton) {
-                    doubleCheckSingleton = new Singleton();
+                    doubleCheckSingleton = new SingletonDemo();
                 }
             }
         }
@@ -50,30 +50,30 @@ public class Singleton {
     // 延迟加载，减少内存开销
     // 延迟加载，线程安全（java中class加载时互斥的），也减少了内存消耗，推荐使用内部类方式。
     private static class SingletonHolder {
-        private static Singleton instance = new Singleton();
+        private static SingletonDemo instance = new SingletonDemo();
     }
     //    private Singleton() {
     //    }
-    public static Singleton getInnerClassInstance() {
+    public static SingletonDemo getInnerClassInstance() {
         return SingletonHolder.instance;
     }
 
     public static void main(String[] args) {
 
         // 测试饿汉式
-        Singleton hungerSingleton = getHungerInstance();
+        SingletonDemo hungerSingleton = getHungerInstance();
         hungerSingleton.method();
 
         // 测试懒汉式
-        Singleton lazySingleton = getLazyInstance();
+        SingletonDemo lazySingleton = getLazyInstance();
         lazySingleton.method();
 
         // 双重线程检查模式
-        Singleton doubleCheckSingleton = getDoubleCheckInstance();
+        SingletonDemo doubleCheckSingleton = getDoubleCheckInstance();
         doubleCheckSingleton.method();
 
         // 内部类模式
-        Singleton innerSingleton = getInnerClassInstance();
+        SingletonDemo innerSingleton = getInnerClassInstance();
         innerSingleton.method();
 
     }
