38d5fbef08904b58cc2334740cd9eef6ca476978
sangbowei <rock.sang@oyohotels.cn>
2019-12-11 17:10:22 +0800

    构造者模式代码完善

---
+++ src/main/java/com/rocksang/study/sharing/demo/designDode/BuilderMode.java
@@ -0,0 +1,115 @@
+package com.rocksang.study.sharing.demo.designDode;
+
+import com.alibaba.fastjson.JSON;
+import lombok.Data;
+
+/**
+ * @Desc: 构造者模式
+ * @Author: Rock.Sang
+ * @Date: 2019/12/11 4:34 PM
+ * @blame Personal
+ */
+public class BuilderMode {
+
+    public static void main(String[] args) {
+        Product product1 = Director.construct("one").retrievePro();
+        Product product2 = Director.construct("two").retrievePro();
+    }
+}
+
+@Data
+class Director{
+    //这里可以结合工厂模式
+    protected static Builder construct(String type){
+        Builder builder = null;
+        if("one".equals(type)){
+            builder=new ConcreteBuilderOne();
+            builder.buildOne();
+            builder.buildTwo();
+            builder.retrievePro();
+        }else if("two".equals(type)){
+            builder=new ConcreteBuilderTwo();
+            builder.buildOne();
+            builder.buildTwo();
+            builder.retrievePro();
+        }
+        return builder;
+    }
+}
+
+// ************************
+interface Builder{
+
+    void buildOne();
+
+    void buildTwo();
+
+    Product retrievePro();
+}
+
+class ConcreteBuilderOne implements Builder{
+
+    private Product product=new ProductOne();
+
+    @Override
+    public void buildOne(){
+        //打造第一个零件
+        product.setPart1("产品1-零件1");
+    }
+    @Override
+    public void buildTwo(){
+        //打造第二个零件
+        product.setPart2("产品1-零件2");
+    }
+
+    @Override
+    public Product retrievePro(){
+        //组装出产品1
+        System.out.println("this is a product, json is "+ JSON.toJSON(product));
+        return product;
+    }
+}
+
+class ConcreteBuilderTwo implements Builder{
+
+    private Product product=new ProductTwo();
+
+    @Override
+    public void buildOne(){
+        //打造第一个零件
+        product.setPart1("产品2-零件1");
+    }
+    @Override
+    public void buildTwo(){
+        //打造第二个零件
+        product.setPart2("产品2-零件2");
+    }
+
+    @Override
+    public Product retrievePro(){
+        //组装出产品2
+        System.out.println("this is a product, json is "+ JSON.toJSON(product));
+        return product;
+    }
+}
+
+// ************************
+@Data
+abstract class Product{
+
+    //产品分为三部分
+
+    String part1;
+
+    String part2;
+
+
+
+}
+class ProductOne extends Product{
+    //具体第一个产品相关属性方法
+}
+
+class ProductTwo extends Product{
+    //具体第二个产品相关属性方法
+}
--- src/main/java/com/rocksang/study/sharing/demo/designDode/SingletonMode.java
+++ src/main/java/com/rocksang/study/sharing/demo/designDode/SingletonMode.java
@@ -5,13 +5,13 @@ package com.rocksang.study.sharing.demo.designDode;
  *
  * @blame Android Team
  */
-public class SingletonDemo {
+public class SingletonMode {
 
     // 饿汉式
-    private static SingletonDemo hangerSingleton = new SingletonDemo();
-    private SingletonDemo() {
+    private static SingletonMode hangerSingleton = new SingletonMode();
+    private SingletonMode() {
     }
-    protected static SingletonDemo getHungerInstance() {
+    protected static SingletonMode getHungerInstance() {
         return hangerSingleton;
     }
 
@@ -19,12 +19,12 @@ public class SingletonDemo {
     // 懒汉式
     // 优点：延迟加载（需要的时候才去加载）,适合单线程操作
     // 缺点： 线程不安全，在多线程中很容易出现不同步的情况，如在数据库对象进行的频繁读写操作时。
-    private static SingletonDemo lazySingleton = null;
+    private static SingletonMode lazySingleton = null;
     //    private Singleton() {
     //    }
-    public static SingletonDemo getLazyInstance() {
+    public static SingletonMode getLazyInstance() {
         if (null == lazySingleton) {
-            lazySingleton = new SingletonDemo();
+            lazySingleton = new SingletonMode();
         }
         return lazySingleton;
     }
@@ -32,14 +32,14 @@ public class SingletonDemo {
     //  双重线程检查模式
     //  优点：线程安全，支持延时加载，调用效率高
     //  缺点： 写法复杂，不简洁
-    private static SingletonDemo doubleCheckSingleton = null;
+    private static SingletonMode doubleCheckSingleton = null;
     //    private Singleton() {
     //    }
-    public static SingletonDemo getDoubleCheckInstance() {
+    public static SingletonMode getDoubleCheckInstance() {
         if (null == doubleCheckSingleton) {
-            synchronized (SingletonDemo.class) {
+            synchronized (SingletonMode.class) {
                 if (null == doubleCheckSingleton) {
-                    doubleCheckSingleton = new SingletonDemo();
+                    doubleCheckSingleton = new SingletonMode();
                 }
             }
         }
@@ -50,30 +50,30 @@ public class SingletonDemo {
     // 延迟加载，减少内存开销
     // 延迟加载，线程安全（java中class加载时互斥的），也减少了内存消耗，推荐使用内部类方式。
     private static class SingletonHolder {
-        private static SingletonDemo instance = new SingletonDemo();
+        private static SingletonMode instance = new SingletonMode();
     }
     //    private Singleton() {
     //    }
-    public static SingletonDemo getInnerClassInstance() {
+    public static SingletonMode getInnerClassInstance() {
         return SingletonHolder.instance;
     }
 
     public static void main(String[] args) {
 
         // 测试饿汉式
-        SingletonDemo hungerSingleton = getHungerInstance();
+        SingletonMode hungerSingleton = getHungerInstance();
         hungerSingleton.method();
 
         // 测试懒汉式
-        SingletonDemo lazySingleton = getLazyInstance();
+        SingletonMode lazySingleton = getLazyInstance();
         lazySingleton.method();
 
         // 双重线程检查模式
-        SingletonDemo doubleCheckSingleton = getDoubleCheckInstance();
+        SingletonMode doubleCheckSingleton = getDoubleCheckInstance();
         doubleCheckSingleton.method();
 
         // 内部类模式
-        SingletonDemo innerSingleton = getInnerClassInstance();
+        SingletonMode innerSingleton = getInnerClassInstance();
         innerSingleton.method();
 
     }
