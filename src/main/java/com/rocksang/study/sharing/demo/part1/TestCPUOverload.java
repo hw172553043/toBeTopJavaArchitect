14600342c8b47f81442b507b0b9add6321eeecbb
sangbowei <rock.sang@oyohotels.cn>
2020-03-20 20:29:22 +0800

    CPU飙高

---
+++ src/main/java/com/rocksang/study/sharing/demo/part1/TestCPUOverload.java
@@ -0,0 +1,19 @@
+package com.rocksang.study.sharing.demo.part1;
+
+public class TestCPUOverload {
+
+
+    public static void main(String[] args) {
+        testCpuOverload();
+    }
+
+    private static void testCpuOverload(){
+
+        long i = 0;
+        while (i>=0){
+
+        }
+
+    }
+
+}
